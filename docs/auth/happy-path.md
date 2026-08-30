32ed1b64c847f85d2e1f3aff1ec6efba5fbd81d9
Nathan <nathanael.sdet.qa.teste@gmail.com>
2026-08-29 21:40:57 -0300

    Add new test specifications and documentation for authentication and cart functionality

---
+++ docs/auth/happy-path.md
@@ -0,0 +1,7 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("Happy Path", () => {
+  test.describe("Positive scenarios", () => {
+    test("", async ({ page }) => {});
+  });
+});
--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: "https://sauce-demo.myshopify.com/collections/all",
+    baseURL: "https://demo.nopcommerce.com/",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
