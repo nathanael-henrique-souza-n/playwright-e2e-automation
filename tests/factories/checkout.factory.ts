91785526204d5e26aa608a503ef3228e93476393
Nathan <nathanael.sdet.qa.teste@gmail.com>
2026-08-31 23:08:43 -0300

    Add checkoutFactory function for generating checkout data

---
+++ tests/factories/checkout.factory.ts
@@ -0,0 +1,16 @@
+import { faker } from "@faker-js/faker";
+import type { checkout } from "../interfaces/checkout.interface";
+
+export function checkoutFactory(overrider: Partial<checkout> = {}) {
+  return {
+    country: faker.helpers.arrayElement(["Brazil"]),
+    State_province: faker.helpers.arrayElement(["New Yourk"]),
+    city: faker.location.city(),
+    Address1: faker.location.streetAddress(),
+    Address2: faker.location.secondaryAddress(),
+    Zip_postalCode: faker.location.zipCode(),
+    PhoneNumber: faker.phone.number(),
+    faxNumber: faker.phone.number(),
+    ...overrider,
+  };
+}
