fd6045b9ef45a8f06126b7045af31bc11a55c917
Nathan <nathanael.sdet.qa.teste@gmail.com>
2026-08-31 23:16:14 -0300

    Add LoginPage class for user authentication functionality

---
+++ tests/POM/login.page.ts
@@ -0,0 +1,13 @@
+import { Page } from "@playwright/test";
+
+import { user_login } from "../interfaces/user.login.interface";
+
+class LoginPage {
+  constructor(private page: Page) {}
+
+  async login(User: user_login) {
+    await this.page.locator('input[id="Email"]').fill(User.email);
+    await this.page.locator('input[id="Password"]').fill(User.password);
+    await this.page.locator(".button-1.login-button").click();
+  }
+}
