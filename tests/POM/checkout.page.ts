0ab2841f87604bc975c2c5a40c8c6975910c6ac0
Nathan <nathanael.sdet.qa.teste@gmail.com>
2026-08-31 23:16:18 -0300

    Add CheckoutPage class for handling checkout functionality

---
+++ tests/POM/checkout.page.ts
@@ -0,0 +1,41 @@
+import { Page, expect } from "@playwright/test";
+
+import { checkout } from "../interfaces/checkout.interface";
+
+class CheckoutPage {
+  constructor(private page: Page) {}
+
+  async Checkout(Checkout: checkout) {
+    await this.page
+      .locator('select[data-trigger="country-select"]')
+      .selectOption(Checkout.country);
+
+    await this.page
+      .locator('select[data-trigger="state-select"]')
+      .selectOption(Checkout.State_province);
+
+    await this.page
+      .locator('input[id="BillingNewAddress_City"]')
+      .fill(Checkout.city);
+
+    await this.page
+      .locator('input[id="BillingNewAddress_Address1"]')
+      .fill(Checkout.Address1);
+
+    await this.page
+      .locator('input[id="BillingNewAddress_Address2"]')
+      .fill(Checkout.Address2);
+
+    await this.page
+      .locator('input[id="BillingNewAddress_ZipPostalCode"]')
+      .fill(Checkout.Zip_postalCode);
+
+    await this.page
+      .locator('input[id="BillingNewAddress_PhoneNumber"]')
+      .fill(Checkout.PhoneNumber.toString());
+
+    await this.page
+      .locator("input[BillingNewAddress_FaxNumber]")
+      .fill(Checkout.faxNumber.toString());
+  }
+}
