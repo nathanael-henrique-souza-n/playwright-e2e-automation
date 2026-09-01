8d3323c674d355a1859d6f148354bd6f9ce00b39
Nathan <nathanael.sdet.qa.teste@gmail.com>
2026-08-31 23:13:45 -0300

    Add checkout interface for checkout data structure

---
+++ tests/interfaces/checkout.interface.ts
@@ -0,0 +1,10 @@
+export interface checkout {
+  country: string;
+  State_province: string;
+  city: string;
+  Address1: string;
+  Address2: string;
+  Zip_postalCode: string;
+  PhoneNumber: number;
+  faxNumber: number;
+}
