436d99ec98b0f25ed4cb099beb1ee8a43c8caa89
Nathan <nathanael.sdet.qa.teste@gmail.com>
2026-09-05 18:44:23 -0300

    Add happy path test for user login with valid credentials

--- tests/auth/happy-path.spec.ts
+++ tests/auth/happy-path.spec.ts
@@ -1,7 +1,17 @@
 import { test, expect } from "@playwright/test";
+import { registerOrLogin } from "../helper/auth/register_Or_login";
 
 test.describe("Happy Path", () => {
   test.describe("Positive scenarios", () => {
-    test("", async ({ page }) => {});
+    test("BR-001 - User should be able to login with valid credentials", async ({
+      page,
+    }) => {
+      await page.goto("/");
+      await page.waitForTimeout(15_000);
+      await registerOrLogin(page);
+
+      console.log(await page.title());
+      console.log(page.url());
+    });
   });
 });
