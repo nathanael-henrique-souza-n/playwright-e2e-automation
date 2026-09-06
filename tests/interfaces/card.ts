359a70b519ca1de276801171769dbe701b0f3507
Nathan <nathanael.sdet.qa.teste@gmail.com>
2026-09-06 11:14:14 -0300

    Refactor card interface to update property names and structure for credit card data

--- tests/interfaces/card.ts
+++ tests/interfaces/card.ts
@@ -1,10 +1,7 @@
-export interface card {
-  credit_card: "Visa" | "Master card" | "Discover" | "Amex";
-  cardholder_name: string;
-  card_number: number;
-  expiration_data: {
-    month: number;
-    years: number;
-  };
-  card_code: number;
+export interface card_data {
+  nameCard: string;
+  numberCard: string;
+  cvcCard: string;
+  expirationMonthCard: number;
+  expirationYearCard: number;
 }
