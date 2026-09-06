2428f31e8fc618391c9d846caefa7d1df2e18674
Nathan <nathanael.sdet.qa.teste@gmail.com>
2026-09-06 11:23:47 -0300

    Refactor happy-path test to remove unused login logic and clean up imports

--- tests/auth/happy-path.spec.ts
+++ tests/auth/happy-path.spec.ts
@@ -1,17 +1,12 @@
 import { test, expect } from "@playwright/test";
-import { registerOrLogin } from "../helper/auth/register_Or_login";
+import { LoginPage } from "../POM/login.page";
+
+import user from "../fixtures/users.json" with { type: "json" };
 
 test.describe("Happy Path", () => {
   test.describe("Positive scenarios", () => {
     test("BR-001 - User should be able to login with valid credentials", async ({
       page,
-    }) => {
-      await page.goto("/");
-      await page.waitForTimeout(15_000);
-      await registerOrLogin(page);
-
-      console.log(await page.title());
-      console.log(page.url());
-    });
+    }) => {});
   });
 });
