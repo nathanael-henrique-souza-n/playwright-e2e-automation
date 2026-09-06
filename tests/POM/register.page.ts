0b2efc434895d97f1d90fc481b10821002c6ab28
Nathan <nathanael.sdet.qa.teste@gmail.com>
2026-09-05 23:40:54 -0300

    Refactor registerPage class to correct user property references and export the class

--- tests/POM/register.page.ts
+++ tests/POM/register.page.ts
@@ -3,20 +3,20 @@ import { expect } from "@playwright/test";
 
 import type { user_register } from "../interfaces/user.register.interfaces";
 
-class registerPage {
+export class registerPage {
   constructor(private page: Page) {}
 
   async register(User: user_register) {
-    await this.page.locator(`input[value="${User.gender}"]`).check();
+    await this.page.locator(`input[value="${User.title}"]`).check();
     await expect(this.page.locator(".button-1.register-button")).toBeVisible();
     await this.page.locator(".button-1.register-button").click();
 
-    await this.page.locator('input[id="FirstName"]').fill(User.firstName);
-    await this.page.locator('input[id="LastName"]').fill(User.lastName);
-    await this.page.locator('input[id="Email"]').fill(User.Email);
-    await this.page.locator('input[id="Company"]').fill(User.companyName);
-    await this.page.locator('input[id="Password"]').fill(User.Password);
-    await this.page.locator('input[id="ComfirmPassword"]').fill(User.Password);
+    await this.page.locator('[inputid="FirstName"]').fill(User.firstName);
+    await this.page.locator('[inputid="LastName"]').fill(User.lastName);
+    await this.page.locator('[inputid="Email"]').fill(User.email);
+    await this.page.locator('input[id="Company"]').fill(User.company);
+    await this.page.locator('input[id="Password"]').fill(User.password);
+    await this.page.locator('input[id="ComfirmPassword"]').fill(User.password);
 
     await expect(this.page.locator(".button-1.register-button")).toBeVisible();
     await this.page.locator(".button-1.register-next-step-button").click();
