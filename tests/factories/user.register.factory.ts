60f73c849dfac41acfe3d52cbd759c98d6a78b9a
Nathan <nathanael.sdet.qa.teste@gmail.com>
2026-09-06 11:22:10 -0300

    Refactor user registration factory to improve structure and add missing fields

--- tests/factories/user.register.factory.ts
+++ tests/factories/user.register.factory.ts
@@ -1,14 +1,41 @@
 import { faker } from "@faker-js/faker";
 import type { user_register } from "../interfaces/user.register.interfaces";
 
-export function userRegister(overrider: Partial<user_register> = {}) {
+export function RegisterUser(
+  overrides: Partial<user_register> = {},
+): user_register {
   return {
-    gender: faker.helpers.arrayElement(["Male", "Female"]),
-    first_name: faker.person.firstName(),
-    last_name: faker.person.lastName(),
+    title: faker.helpers.arrayElement(["Mr", "Mrs"]),
+    name: faker.person.fullName(),
     email: faker.internet.email(),
-    company_name: faker.company.name(),
-    password: faker.internet.password(),
-    ...overrider,
+    password: "Password@123",
+
+    birthdata: {
+      day: "15",
+      month: "5",
+      year: "2000",
+    },
+
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+
+    company: faker.company.name(),
+    address: faker.location.streetAddress(),
+    address2: faker.location.secondaryAddress(),
+
+    country: faker.helpers.arrayElement([
+      "India",
+      "United States",
+      "Canada",
+      "Australia",
+      "Israel",
+      "New Zealand",
+      "Singapore",
+    ]),
+    state: faker.location.state(),
+    city: faker.location.city(),
+    zipCode: faker.location.zipCode(),
+    mobileNumber: faker.phone.number(),
+    ...overrides,
   };
 }
