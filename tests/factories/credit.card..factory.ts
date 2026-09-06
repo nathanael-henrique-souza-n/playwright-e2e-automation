15e174d312614ed17e36a8ad39b52fdf64fe12b7
Nathan <nathanael.sdet.qa.teste@gmail.com>
2026-09-06 11:23:40 -0300

    Refactor credit card factory to use validCard function and simplify structure

--- tests/factories/credit.card..factory.ts
+++ tests/factories/credit.card..factory.ts
@@ -1,25 +1,13 @@
 import { faker } from "@faker-js/faker";
-import type { card } from "../interfaces/card";
+import type { card_data } from "../interfaces/card";
 
-export function card(overrider: Partial<card> = {}) {
+export function validCard(overrides: Partial<card_data> = {}): card_data {
   return {
-    credit_card: faker.helpers.arrayElement([
-      "Visa",
-      "Master card",
-      "Discover",
-      "Amex",
-    ]),
-    cardholder_name: faker.person.fullName(),
-    card_number: faker.finance.creditCardNumber(),
-    expiration_data: {
-      month: faker.helpers.arrayElement([
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
-      ]),
-      years: faker.helpers.arrayElement([
-        2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030,
-      ]),
-    },
-    card_code: faker.number.int({ min: 100, max: 999 }),
-    ...overrider,
+    nameCard: faker.person.fullName(),
+    numberCard: faker.finance.creditCardNumber(),
+    cvcCard: faker.finance.creditCardCVV(),
+    expirationMonthCard: 12,
+    expirationYearCard: 2030,
+    ...overrides,
   };
 }
