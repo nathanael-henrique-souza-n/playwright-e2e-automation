911ef0b1107d4d070aba8fd8a4010a205a6fb5f3
Nathan <nathanael.sdet.qa.teste@gmail.com>
2026-09-08 23:45:03 -0300

    Remove negative scenario tests for unregistered user login attempt

--- tests/auth/login/happy-path.spec.ts
+++ tests/auth/login/happy-path.spec.ts
@@ -20,17 +20,4 @@ test.describe("Happy Path", () => {
       ).toBeVisible();
     });
   });
-
-  test.describe("Negative scenarios", () => {
-    test("BR-002 - User cannot log in with unregistered credential", async ({
-      page,
-    }) => {
-      let unregisteredUser = userLogin();
-      await login(page, unregisteredUser);
-
-      await expect(
-        await page.getByText("Your email or password is incorrect!"),
-      ).toBeVisible();
-    });
-  });
 });
