e68e3af7376d7c8f36a50abc9d0f83edc9341527
Nathan <nathanael.sdet.qa.teste@gmail.com>
2026-09-09 00:16:50 -0300

    Update registration test to reflect valid user data scenario

--- tests/auth/signup/happy-path.spec.ts
+++ tests/auth/signup/happy-path.spec.ts
@@ -1,11 +1,11 @@
 import { test, expect } from "@playwright/test";
 import { LoginPage } from "../../POM/login.page";
 
-import user from "../../fixtures/users.json" with { type: "json" };
+import type { user_register } from "../../interfaces/user.register.interfaces";
 
 test.describe("Happy Path", () => {
   test.describe("Positive scenarios", () => {
-    test("BR-001 - User should be able to login with valid credentials", async ({
+    test("BR-001 - Register a new user with valid data successfully", async ({
       page,
     }) => {});
   });
