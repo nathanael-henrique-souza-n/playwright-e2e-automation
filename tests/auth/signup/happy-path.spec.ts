70f13e1cd44318f02c0397d2139fc7ce5441b5d3
Nathan <nathanael.sdet.qa.teste@gmail.com>
2026-09-11 00:06:20 -0300

    Implement positive scenario test for user registration with valid data

--- tests/auth/signup/happy-path.spec.ts
+++ tests/auth/signup/happy-path.spec.ts
@@ -1,12 +1,44 @@
 import { test, expect } from "@playwright/test";
-import { LoginPage } from "../../POM/login.page";
 
-import type { user_register } from "../../interfaces/user.register.interfaces";
+import { RegisterUser } from "../../factories/user.register.factory";
+import { register } from "../../helper/auth/register";
 
 test.describe("Happy Path", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/login");
+  });
+
   test.describe("Positive scenarios", () => {
     test("BR-001 - Register a new user with valid data successfully", async ({
       page,
-    }) => {});
+    }) => {
+      let accountCreated = [
+        { element: '[data-qa="account-created"]', text: "Account Created!" },
+        {
+          element: "p",
+          text: "Congratulations! Your new account has been successfully created!",
+        },
+        {
+          element: "p",
+          text: "You can now take advantage of member privileges to enhance your online shopping experience with us.",
+        },
+      ];
+
+      const registerUser = RegisterUser();
+
+      await page.locator('[data-qa="signup-name"]').fill(registerUser.name);
+      await page.locator('[data-qa="signup-email"]').fill(registerUser.email);
+      await page.locator('[data-qa="signup-button"]').click();
+      await register(page, registerUser);
+
+      for (let index of accountCreated) {
+        const element = page
+          .locator(index.element)
+          .filter({ hasText: index.text });
+
+        await expect(element).toBeVisible();
+        await expect(element).toContainText(index.text);
+      }
+    });
   });
 });
